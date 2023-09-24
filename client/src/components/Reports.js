6cdcc11d7b39a0ff7429420f6ced04e84b0386d9
Rayan-Weragala <110955943+Rayan-Weragala@users.noreply.github.com>
2023-09-24 22:47:10 +0530

    Reports file added

---
+++ client/src/components/Reports.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import CommonLayout from './CommonLayout'
+const Reports = () => {
+  return (
+    <CommonLayout>
+    <div></div>
+    </CommonLayout>
+  )
+}
+
+export default Reports
