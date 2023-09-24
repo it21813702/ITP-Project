30a21ecdef3e47c071a43904412544e77eb35af1
Rayan-Weragala <110955943+Rayan-Weragala@users.noreply.github.com>
2023-09-24 23:47:30 +0530

    navbar changed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Tabs,
+  TextField,
+  InputAdornment, // Import InputAdornment
+} from '@mui/material';
 
-const Navbar = () => {
+import { Search as SearchIcon } from '@mui/icons-material'; // Import the search icon
+
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  const [value, setValue] = useState();
+
   return (
-    <div></div>
-  )
-}
+    <div>
+      <AppBar sx={{ background: '#219C90' }} position='sticky'>
+        <Toolbar>
+          <NavLink to='/' style={{ color: 'white' }}>
+            <Typography>
 
-export default Navbar
+            </Typography>
+          </NavLink>
+          <Tabs
+            sx={{ ml: 'auto' }}
+            textColor='inherit'
+            indicatorColor='primary'
+            value={value}
+            onChange={(e, val) => setValue(val)}
+          >
+            
+          </Tabs>
+
+          
+          <TextField
+            placeholder="Search"
+            variant="outlined"
+            size="small"
+            sx={{ marginLeft: '10px',background:'white' }} 
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;
